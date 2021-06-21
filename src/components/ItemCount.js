479a530af99b74b4bebbe27da0d9bc12272453d4
Agustina Magnalardo <agusmagna@MacBook-Pro-de-AgustinaMagna.local>
2021-06-21 15:33:30 -0300

    contador

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -40,22 +40,3 @@ function ItemCount ({stock, onAdd, initial}) {
 
 
 export default ItemCount
-
-//
-// import { Card } from 'antd';
-//
-// ReactDOM.render(
-//   <>
-//     <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-//       <p>Card content</p>
-//       <p>Card content</p>
-//       <p>Card content</p>
-//     </Card>
-//     <Card size="small" title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-//       <p>Card content</p>
-//       <p>Card content</p>
-//       <p>Card content</p>
-//     </Card>
-//   </>,
-//   mountNode,
-// );
